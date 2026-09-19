df7278aad146c7b227cb84e4cd4f730bf2c1a00e
Sintu Kumar <shiva49767@gmail.com>
2026-09-19 13:35:42 +0530

    Create a browser router and provide to childrens

--- mailHub/src/App.jsx
+++ mailHub/src/App.jsx
@@ -1,12 +1,32 @@
 
 import './App.css'
+import Home from './components/Home';
+import Inbox from './components/Inbox'
+import Mail from './components/Mail';
 import Navbar from './components/Navbar'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([{
+  path:"/",
+element:<Home/>,
+children:[
+  {
+    path:'/',
+    element:<Inbox/>
+  },
+  {
+    path:'/mail/:id',
+    element:<Mail/>
+  }
+]
+}
+])
 
 function App() {
   
   return (
     <div className=' min-h-screen min-w-screen overflow-hidden bg-[#F8FAFD]'>
     <Navbar/>
+    <RouterProvider router={router}/>
     </div>
   )
 }
