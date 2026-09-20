521c6da6f7b1f6a1aebb2ebe0d85b62f22728b88
Sintu Kumar <shiva49767@gmail.com>
2026-09-20 14:11:28 +0530

    add the hardcode page navigation and import the messages

--- mailHub/src/components/Inbox.jsx
+++ mailHub/src/components/Inbox.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { FaCaretDown } from 'react-icons/fa6'
-import { MdCropSquare,MdInbox } from 'react-icons/md'
+import { MdCropSquare, MdInbox, MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
 import { IoMdRefresh, IoMdMore } from "react-icons/io";
 import { GoTag } from "react-icons/go";
 import { FaUserFriends } from "react-icons/fa";
 import { RiErrorWarningLine } from "react-icons/ri";
 import { useState } from 'react';
-const mailType=[
-  {text:'Primary',icon:<MdInbox size={20}/> },
-{text:'Promotions',icon:<GoTag size={20}/> },
-{text:'Social',icon:<FaUserFriends size={20}/> },
-{text:'Updates',icon:<RiErrorWarningLine size={20} className='transform rotate-180'/> }
+import Messages from '../Messages';
+const mailType = [
+  { text: 'Primary', icon: <MdInbox size={20} /> },
+  { text: 'Promotions', icon: <GoTag size={20} /> },
+  { text: 'Social', icon: <FaUserFriends size={20} /> },
+  { text: 'Updates', icon: <RiErrorWarningLine size={20} className='transform rotate-180' /> }
 ]
 const Inbox = () => {
-  const [mailTypeSelected,setmailTypeSelected]= useState(0);
+  const [mailTypeSelected, setmailTypeSelected] = useState(0);
   return (
     <div className='flex-1 bg-white rounded-xl mx-5'>
       <div className='flex items-center justify-between px-4'>
@@ -29,24 +30,29 @@ const Inbox = () => {
             <IoMdMore size={20} />
           </div>
         </div>
-      </div>
-        <div className='h-[80vh] overflow-y-auto'>
-          <div className='flex items-center gap-1'>
-        {mailType.map((items,index)=>{
-          // const Icon = items.icon;
-          return(
-            <div key={index} 
-            onClick={()=>setmailTypeSelected(index)}
-            className={`${mailTypeSelected==index?'border-b-4 border-b-blue-500':'border-b-4 border-b-transparent'} flex items-center w-60 p-3 mt-2  rounded-sm cursor-pointer gap-5 hover:bg-gray-100`}>
-               {items.icon}
-              <span >{items.text}</span>
-            </div>
-
-          )
-        })}
-
-          </div>
+        <div className='flex gap-2'>
+          <p className='text-gray-600 text-sm py-2 px-3  rounded-sm hover:bg-gray-100  hover:border-2 hover:cursor-pointer'>1-50 of 100</p>
+          <button><MdKeyboardArrowLeft size={20}/></button>
+          <button><MdKeyboardArrowRight size={20}/></button>
         </div>
+      </div>
+      <div className='h-[80vh] overflow-y-auto'>
+        <div className='flex items-center gap-1'>
+          {mailType.map((items, index) => {
+            // const Icon = items.icon;
+            return (
+              <div key={index}
+                onClick={() => setmailTypeSelected(index)}
+                className={`${mailTypeSelected == index ? 'border-b-4 border-b-blue-500' : 'border-b-4 border-b-transparent'} flex items-center w-60 p-3 mt-2  rounded-sm cursor-pointer gap-5 hover:bg-gray-100`}>
+                {items.icon}
+                <span >{items.text}</span>
+              </div>
+
+            )
+          })}
+        </div>
+        <Messages />
+      </div>
     </div>
   )
 }
